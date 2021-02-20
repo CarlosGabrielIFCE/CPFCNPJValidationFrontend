c7074548be20fd679493e4f5d9bf455d7fe315a8
Carlos Gabriel <carloscms2009@gmail.com>
2021-02-20 18:50:08 -0300

    Deploying without proxy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
@@ -31,6 +33,7 @@ import { PaymentCardComponent } from '../components/payment-card/payment-card.co
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
     MatGridListModule,
